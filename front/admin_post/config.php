bad79d716f9c2f0ad1246113e61b8d66243d6a06
subhush <158732472+subhush@users.noreply.github.com>
2024-02-19 19:16:57 +0530

    Delete front directory

--- front/admin_post/config.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-$conn = mysqli_connect("localhost","root","","duniyagyan") or die ("Connection Faile : " . mysqli_connect_error());
-?>
--- front/admin_post/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
- include 'config.php';
- session_start();
- session_unset();
- session_destroy();
-
- header("location: http://localhost/new1/front/index.php");
-
-?>
